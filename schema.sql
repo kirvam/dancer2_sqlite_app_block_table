51c7fe9703eab705c5a83ed36ab953111eeace31
kirvam <pmphaigh@gmail.com>
2022-01-16 23:17:52 +0000

    added db_enties

--- schema.sql
+++ schema.sql
@@ -7,3 +7,16 @@ create table if not exists entries (
     text string not null,
     status string not null
   );
+
+create table if not exists db_entries (
+    id integer primary key autoincrement,
+    parent string not null,
+    entryParent string not null,
+    entryType string not null,
+    handDate string not null, 
+    entryTitle string not null,
+    authorName string not null,
+    textNote string not null,
+    sqlDate text
+  );
+
